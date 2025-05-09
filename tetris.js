d7e40e5fc563ecba7224522734ce7cab9c7c00d8
super_man2775 <elian.claes@gmail.com>
2025-05-09 09:48:53 +0200

    Update tetris.js

--- tetris.js
+++ tetris.js
@@ -295,6 +295,26 @@ class Piece {
       Z: [
         [1, 1, 0],
         [0, 1, 1],
-        [0, 
+        [0, 0, 0],
+      ],
+    };
+    return shapes[type];
+  }
+
+  getColor(type) {
+    const colors = {
+      I: '#00f0f0',
+      J: '#0000f0',
+      L: '#f0a000',
+      O: '#f0f000',
+      S: '#00f000',
+      T: '#a000f0',
+      Z: '#f00000',
+    };
+    return colors[type];
+  }
+}
+
+
 ::contentReference[oaicite:21]{index=21}
  
